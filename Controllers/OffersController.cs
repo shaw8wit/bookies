66106dc963abfe3ae08db2cdffb9030746fd294f
Divyansh1808 <divyanshkhare1808@gmail.com>
2021-05-21 20:15:36 -0700

    Bug Fixed.

---
+++ Controllers/OffersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bookies.Controllers
+{
+    public class OffersController : Controller
+    {
+        // GET: Offer
+        public ActionResult OffersView()
+        {
+            return View();
+        }
+    }
+}
